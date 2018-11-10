8fb32c14777451f687c06cc5f1158e0dfed4688b
= <=>
2018-11-10 18:50:22 -0500

    Added Ability to Pull a List of Tags

--- src/audit.php
+++ src/audit.php
@@ -2,15 +2,39 @@
 
 namespace ISAudit;
 
+require __DIR__ . '/../vendor/autoload.php';
+
 /**
  * Audit Class
  */
-class Audit extends infusionsoft
+class Audit extends \Infusionsoft\Infusionsoft
 {
+    /**
+     * @var A List of all Tags in our App
+     */
+    private $tags;
 
-    function __construct(argument)
+    function __construct($config = [])
     {
+        parent::__construct($config);
+    }
+
+    function pullTags(){
         /* TODO: Pull a list of all tags */
+        $table = 'ContactGroup';
+        $limit = 100;
+        $page = 0;
+        $orderBy = 'Id';
+        $ascending = true;
+
+        $returnFields = array('GroupName');
+        $queryData = array('Id' => '%');
+        $selectedFields = array('GroupName', 'Id');
+        $this->tags = $this->data()->query($table, $limit, $page, $queryData, $selectedFields, $orderBy, $ascending);
+    }
+    function showTags(){
+        return $this->tags;
+    }
         
         
          /* TODO: Get tag applied table */
@@ -20,9 +44,4 @@ class Audit extends infusionsoft
         
          /* TODO: Compare to see when last used, how many contacts have that tag etc */
         
-        
-    }
-
-
-
 }
